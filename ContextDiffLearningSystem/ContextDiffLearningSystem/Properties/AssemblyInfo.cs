f219c377e6ca2bc4c86a9f38b6fe06b640fbda7b
inobo55 <inobo52@gmail.com>
2014-02-05 17:34:38 +0900

    開発開始
    はじめてのコミット

---
+++ ContextDiffLearningSystem/ContextDiffLearningSystem/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+
+// アセンブリに関する一般情報は、以下の属性セットによって 
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
+[assembly: AssemblyTitle("ContextDiffLearningSystem")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ContextDiffLearningSystem")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
+// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、 
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID がタイプ ライブラリの ID になります。
+[assembly: Guid("b760b00d-8e29-4f47-9b96-7055686b3f87")]
+
+// アセンブリのバージョン情報は、以下の 4 つの値で構成されています。
+//
+//      メジャー バージョン
+//      マイナー バージョン 
+//      ビルド番号
+//      リビジョン
+//
+// すべての値を指定するか、以下のように '*' を使ってビルドおよびリビジョン番号を
+// 既定値にすることができます。
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
